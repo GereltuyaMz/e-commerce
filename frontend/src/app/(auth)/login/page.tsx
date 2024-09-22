79566902cd7707b265c38b7365c4c9c5a1cc2564
GereltuyaMz <gegiimz96@gmail.com>
2024-09-22 14:45:03 +0800

    create forget password page & steps

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -4,41 +4,44 @@ import Link from "next/link";
 import React from "react";
 
 const LogIn = () => {
-	return (
-		<div className="h-[700px] flex flex-col justify-center items-center bg-layer">
-			<div className="w-[320px]">
-				<h1 className="text-2xl font-semibold mb-8 text-center">Нэвтрэх</h1>
-				<div className="flex flex-col gap-4 text-sm">
-					<Input
-						type="email"
-						placeholder="Имэйл хаяг"
-						className="rounded-[18px] bg-white shadow-sm"
-					/>
-					<Input
-						type="password"
-						placeholder="Нууц үг"
-						className="rounded-[18px] bg-white shadow-sm"
-					/>
-					<Button className="bg-blue-primary rounded-[18px]">Нэвтрэх</Button>
-				</div>
-				<div className="flex flex-col gap-12 mt-4">
-					<Link href="/forgetpass" className="text-center text-sm">
-						<Button className="underline text-muted-foreground font-normal" variant="link">
-							Нууц үг мартсан
-						</Button>
-					</Link>
-					<Link href="/signup">
-						<Button
-							variant="outline"
-							className="border-blue-primary text-blue-primary rounded-[18px] w-full bg-white"
-						>
-							Бүртгүүлэх
-						</Button>
-					</Link>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="h-[700px] flex flex-col justify-center items-center">
+      <div className="w-[320px]">
+        <h1 className="text-2xl font-semibold mb-8 text-center">Нэвтрэх</h1>
+        <div className="flex flex-col gap-4 text-sm">
+          <Input
+            type="email"
+            placeholder="Имэйл хаяг"
+            className="rounded-[18px] bg-white shadow-sm"
+          />
+          <Input
+            type="password"
+            placeholder="Нууц үг"
+            className="rounded-[18px] bg-white shadow-sm"
+          />
+          <Button className="bg-blue-primary rounded-[18px]">Нэвтрэх</Button>
+        </div>
+        <div className="flex flex-col gap-12 mt-4">
+          <Link href="/forgetpass/email" className="text-center text-sm">
+            <Button
+              className="underline text-muted-foreground font-normal"
+              variant="link"
+            >
+              Нууц үг мартсан
+            </Button>
+          </Link>
+          <Link href="/signup">
+            <Button
+              variant="outline"
+              className="border-blue-primary text-blue-primary rounded-[18px] w-full bg-white"
+            >
+              Бүртгүүлэх
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LogIn;
--- frontend/src/icons/index.ts
+++ frontend/src/icons/index.ts
@@ -2,3 +2,4 @@ export * from "./facebook-icon";
 export * from "./instagram-icon";
 export * from "./linkedIn-icon";
 export * from "./twitter-icon";
+export * from "./email-icon";
