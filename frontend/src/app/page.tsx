619d116e196278d197e278062f5bc5901309a7fd
GereltuyaMz <gegiimz96@gmail.com>
2024-10-01 20:19:09 +0800

    fetch products data from backend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,36 +1,53 @@
 import { Hero } from "@/components/home";
 import { ProductCard, FeaturedProductCard } from "@/components/product-card";
-import { products } from "@/lib/data";
+import { Product } from "@/lib/data";
+import { apiUrl } from "@/lib/utils";
+import axios from "axios";
 
-export default function Home() {
-  return (
-    <main>
-      <Hero />
-      <section className="mt-6 mb-24 max-w-[1100px] mx-auto grid grid-cols-4 gap-y-12 gap-x-5">
-        {products.map((product, index) => {
-          return (
-            <>
-              {index === 6 || index === 7 ? (
-                <FeaturedProductCard
-                  key={index}
-                  name={product.name}
-                  price={product.price}
-                  image={product.image}
-                  discount={product.discount}
-                />
-              ) : (
-                <ProductCard
-                  key={index}
-                  name={product.name}
-                  price={product.price}
-                  image={product.image}
-                  discount={product.discount}
-                />
-              )}
-            </>
-          );
-        })}
-      </section>
-    </main>
-  );
+interface ProductResponse {
+	products: Product[];
+}
+
+const getProductData = async () => {
+	try {
+		const response = await axios.get<ProductResponse>(`${apiUrl}/product`);
+		return response.data.products;
+	} catch (error) {
+		console.error("Error fetching data:", error);
+		throw new Error("Failed to fetch data");
+	}
+};
+
+export default async function Home() {
+	const products = await getProductData();
+	return (
+		<main>
+			<Hero />
+			<section className="mt-6 mb-24 max-w-[1100px] mx-auto grid grid-cols-4 gap-y-12 gap-x-5">
+				{products.map((product, index) => {
+					return (
+						<>
+							{index === 6 || index === 7 ? (
+								<FeaturedProductCard
+									key={index}
+									name={product.name}
+									price={product.price}
+									images={product.images}
+									discount={product.discount}
+								/>
+							) : (
+								<ProductCard
+									key={index}
+									name={product.name}
+									price={product.price}
+									images={product.images}
+									discount={product.discount}
+								/>
+							)}
+						</>
+					);
+				})}
+			</section>
+		</main>
+	);
 }
